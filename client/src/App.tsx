59f2815e36c8e21369eba5cb4c22dc4c9d0afc65
Marxan Sheikh <marzan3698@gmail.com>
2026-01-18 02:25:48 +0600

    Fix React import error: Use useEffect hook directly instead of React.useEffect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { SidebarProvider } from './contexts/SidebarContext';
@@ -59,7 +60,7 @@ import Sitemap from './pages/public/Sitemap';
 
 function App() {
   // Redirect /install to home immediately on mount
-  React.useEffect(() => {
+  useEffect(() => {
     if (window.location.pathname === '/install') {
       window.location.replace('/');
     }
